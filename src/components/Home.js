2ae914b5714db719e46ae5b47e4f3d3e701d370e
Petteri Sippala <t1sipe00@students.oamk.fi>
2022-10-07 19:30:09 +0300

    spa toimii

---
+++ src/components/Home.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function Home() {
+    return (
+        <div>
+            Tämä on kotisivu
+        </div>
+    )
+}
---
+++ src/components/NotFound.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+
+export default function About(){
+    return (
+        <div>
+       Sivua ei ole olemassa!
+    </div>
+    )
+}
